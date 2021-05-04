3e85766986f67ed4f139e2fdeb970319d4ca39aa
BroJack0809 <79563574+BroJack0809@users.noreply.github.com>
2021-05-05 00:34:48 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import HelloCGU from './cgu_hello';
+import MultiButton from './Buttom';
+function App() {
+  return (
+    <div className="App">
+      <div>
+        {HelloCGU()}
+      </div>
+      <div>
+        {MultiButton(10)}
+      </div>
+    </div>
+  );
+}
+export default App;
